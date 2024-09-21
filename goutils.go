3735a31f0f214b631e4294e0b6bd619ac8801c83
Mikhail Fonaryov <fonicemiha@mail.ru>
2024-09-21 23:11:04 +0300

    modified:   goutils.go

--- goutils.go
+++ goutils.go
@@ -1,9 +1,10 @@
-// Package goutils this is my first test package
+// Package goutils this is my first test package.
 package goutils
 
 import "fmt"
 
-// GoutilsHello is goutils test function
+// GoutilsHello is goutils test function.
+// It is test description.
 func GoutilsHello() {
 	fmt.Println("This is utils")
 }
